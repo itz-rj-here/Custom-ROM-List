06cdd0bcb74c759f6f344fb467d78c029fffab14
It'z RJ <86042583+itz-rj-here@users.noreply.github.com>
2024-05-12 17:36:14 +0600

    Removed Blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,13 +47,6 @@ const config = {
           editUrl:
             'https://github.com/itz-rj-here/Custom-ROM-List/edit/main/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/itz-rj-here/Custom-ROM-List/edit/main/',
-        },
         theme: {
           customCss: './src/css/custom.css',
         },
